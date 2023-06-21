a539a3ba31866591fd84d1e4e49f6c230d562c55
aaaaa ttttt <ryudai.0728@gmail.com>
2023-06-21 16:15:08 +0900

    プロジェクト ファイルを追加します。

---
+++ Matrixfunction.h
@@ -0,0 +1,48 @@
+#pragma once
+
+typedef struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+
+typedef struct Matrix4x4 {
+	float m[4][4];
+};
+
+// 積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+// 平行移動
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float heght, float minDepth, float maxDepth);
+
+
+/*---------------------------------
+ 回転行列
+------------------------------------*/
+
+// X軸
+Matrix4x4 MakeRotateXMatrix(float radian);
+// Y軸
+Matrix4x4 MakeRotateYMatrix(float radian);
+// Z軸
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate);
+
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+//正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+// 逆行列
+Matrix4x4 Inverse(const Matrix4x4& m);
+
+// 単位行列
+Matrix4x4 MakeIdentityMatrix();
---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <Novice.h>
+#include <stdint.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <cmath>
+#include <cassert>
+#include <imgui.h>
+#include "Matrixfunction.h"
+
+// 4x4行列表示
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+			    x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+// 三次元ベクトル表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
