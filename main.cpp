6ee2910e91ce275dbf44f84669dd5ae18b218fcd
aaaaa ttttt <ryudai.0728@gmail.com>
2023-07-10 15:32:23 +0900

    02_01_確認課題

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@
 #include <imgui.h>
 #include "Mathfunction.h"
 
+struct Sphere {
+	Vector3 center;
+	float radius;
+};
+
 // 4x4行列表示
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
@@ -50,6 +55,12 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 		// 線を引く
 		Novice::DrawLine(
 		    (int)screenXsp.x, (int)screenXsp.y, (int)screenXep.x, (int)screenXep.y, WHITE);
+
+		if (xIndex == 5) {
+			// 線を引く
+			Novice::DrawLine(
+			    (int)screenXsp.x, (int)screenXsp.y, (int)screenXep.x, (int)screenXep.y, BLACK);
+		}
 	}
 	// 左から右に順番に引く
 	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
@@ -67,7 +78,94 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 		// 線を引く
 		Novice::DrawLine(
 		    (int)screenZsp.x, (int)screenZsp.y, (int)screenZep.x, (int)screenZep.y, WHITE);
+		if (zIndex == 5) {
+			// 線を引く
+			Novice::DrawLine(
+			    (int)screenZsp.x, (int)screenZsp.y, (int)screenZep.x, (int)screenZep.y, BLACK);
+		}
 	}
+}
+
+// Sphere
+void DrawSphere(
+    const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix,
+    uint32_t color) {
+	const uint32_t kSubdivision = 16;
+	const float pi = (float)M_PI;
+	const float kLonEvery = pi * 2.0f / float(kSubdivision);
+	const float kLatEvery = pi / float(kSubdivision);
+
+	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex) {
+		// 緯度の方向に分割
+		float lat = -pi / 2.0f + kLatEvery * latIndex;
+
+		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex) {
+			// 経度の方向に分割
+			float lon = lonIndex * kLonEvery;
+			// world座標
+			Vector3 a, b, c;
+			// 緯度、経度
+			float latD = pi / kSubdivision;
+			float lonD = (2.0f * pi) / kSubdivision;
+
+			a = {
+			    sphere.center.x + sphere.radius * (std::cos(lat) * std::cos(lon)),
+			    sphere.center.y + sphere.radius * std::sin(lat),
+			    sphere.center.z + sphere.radius * (std::cos(lat) * std::sin(lon))};
+
+			b = {
+			    sphere.center.x + sphere.radius * (std::cos(lat + latD) * std::cos(lon)),
+			    sphere.center.y + sphere.radius * std::sin(lat + latD),
+			    sphere.center.z + sphere.radius * (std::cos(lat + latD) * std::sin(lon))};
+
+			c = {
+			    sphere.center.x + sphere.radius * (std::cos(lat) * std::cos(lon + lonD)),
+			    sphere.center.y + sphere.radius * std::sin(lat),
+			    sphere.center.z + sphere.radius * (std::cos(lat) * std::sin(lon + lonD))};
+
+			// a,b,cをscreen座標に変換
+			Vector3 ndcVertexA = Transform(a, viewProjectionMatrix);
+			Vector3 ndcVertexB = Transform(b, viewProjectionMatrix);
+			Vector3 ndcVertexC = Transform(c, viewProjectionMatrix);
+			Vector3 screenA = Transform(ndcVertexA, viewportMatrix);
+			Vector3 screenB = Transform(ndcVertexB, viewportMatrix);
+			Vector3 screenC = Transform(ndcVertexC, viewportMatrix);
+			// ab acで線を引く
+			Novice::DrawLine((int)screenA.x, (int)screenA.y, (int)screenB.x, (int)screenB.y, color);
+			Novice::DrawLine((int)screenA.x, (int)screenA.y, (int)screenC.x, (int)screenC.y, color);
+		}
+	}
+}
+
+// 長さ（ノルム）
+float Length(const Vector3& v) {
+	float result;
+	result = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return result;
+}
+
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
+}
+
+// 球と球の衝突判定
+bool IsCollision(const Sphere& s1, const Sphere& s2) { 
+	// 中心の距離
+	float distance = Length(Subtract(s2.center, s1.center));
+	// 半径の合計よりも短ければ衝突
+	if (distance <= s1.radius + s2.radius) {
+		return true;
+
+	} else {
+	
+		return false;
+	}
+
 }
 
 const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3";
@@ -82,6 +180,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	Vector3 scale{1.0f, 1.0f, 1.0f};
+	Vector3 rotate{0.0f, 0.0f, 0.0f};
+	Vector3 translate{0.0f, 0.0f, 0.0f};
+	Vector3 cameraScale{1.0f, 1.0f, 1.0f};
+	Vector3 cameraRotate{0.3f, 0.0f, 0.0f};
+	Vector3 cameraTranslate{0.0f, 2.0f, -6.0f};
+
+	Sphere sphere;
+	sphere.center = {0.0f, 0.0f, 0.0f};
+	sphere.radius = 0.5f;
+
+	Sphere sphere2;
+	sphere2.center = {1.0f, 0.0f, 0.0f};
+	sphere2.radius = 0.2f;
+
+	int color = WHITE;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -95,6 +210,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraScale, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix =
+		    MakePerspectiveFovMatrix(0.45f, float(1280) / float(720), 0.1f, 100.0f);
+		// WVPMatrix
+		Matrix4x4 worldViewProjectionMatrix =
+		    Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		// ViewPortMatrix
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, 1200.0f, 720.0f, 0.0f, 1.0f);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -102,6 +228,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+		/// 
+		
+		// imgui
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::DragFloat3("Sphere[0].center", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("Sphere[0].radius", &sphere.radius, 0.01f);
+		ImGui::DragFloat3("Sphere[1].center", &sphere2.center.x, 0.01f);
+		ImGui::DragFloat("Sphere[1].radius", &sphere2.radius, 0.01f);
+		ImGui::End();
+
+		// グリッド
+		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
+		// 球
+		if (IsCollision(sphere, sphere2)) {
+			color = RED;
+		} else {
+			color = WHITE;
+		}
+
+		DrawSphere(sphere, worldViewProjectionMatrix, viewportMatrix, color);
+		DrawSphere(sphere2, worldViewProjectionMatrix, viewportMatrix, WHITE);
 
 		///
 		/// ↑描画処理ここまで
