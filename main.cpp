d8c117857a162a21c504437a817c21aac8174b2a
aaaaa ttttt <ryudai.0728@gmail.com>
2023-08-18 18:32:09 +0900

    02_06_確認課題

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@
 #include <cassert>
 #include <imgui.h>
 #include "Mathfunction.h"
+#include <algorithm>	
 
 struct Sphere {
 	Vector3 center;
 	float radius;
+};
+
+struct AABB {
+	Vector3 min; // 最小点
+	Vector3 max; // 最大点
 };
 
 // 4x4行列表示
@@ -126,9 +132,96 @@ void DrawSphere(
 	}
 }
 
+// AABBの描画
+void DrawAABB(
+    const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix,
+    uint32_t color) {
+
+	Vector3 vertex[8];
+
+	Vector3 ndcVertex[8];
+
+	Vector3 screenVertices[8];
+
+	vertex[0] = {aabb.min.x, aabb.min.y, aabb.min.z};
+	vertex[1] = {aabb.min.x, aabb.min.y, aabb.max.z};
+
+	vertex[2] = {aabb.max.x, aabb.min.y, aabb.min.z};
+	vertex[3] = {aabb.max.x, aabb.min.y, aabb.max.z};
+
+	vertex[4] = {aabb.min.x, aabb.max.y, aabb.min.z};
+	vertex[5] = {aabb.min.x, aabb.max.y, aabb.max.z};
+
+	vertex[6] = {aabb.max.x, aabb.max.y, aabb.min.z};
+	vertex[7] = {aabb.max.x, aabb.max.y, aabb.max.z};
+
+	for (uint32_t Index = 0; Index < 8; ++Index) {
+		// 正規化デバイス座標系
+		ndcVertex[Index] = Transform(vertex[Index], viewProjectionMatrix);
+		// スクリーン座標系
+		screenVertices[Index] = Transform(ndcVertex[Index], viewportMatrix);
+	}
+
+	// 底辺
+	Novice::DrawLine(
+	    int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[1].x),
+	    int(screenVertices[1].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[2].x),
+	    int(screenVertices[2].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[1].x), int(screenVertices[1].y), int(screenVertices[3].x),
+	    int(screenVertices[3].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[2].x), int(screenVertices[2].y), int(screenVertices[3].x),
+	    int(screenVertices[3].y), color);
+
+	// 上辺
+	Novice::DrawLine(
+	    int(screenVertices[4].x), int(screenVertices[4].y), int(screenVertices[5].x),
+	    int(screenVertices[5].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[4].x), int(screenVertices[4].y), int(screenVertices[6].x),
+	    int(screenVertices[6].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[5].x), int(screenVertices[5].y), int(screenVertices[7].x),
+	    int(screenVertices[7].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[6].x), int(screenVertices[6].y), int(screenVertices[7].x),
+	    int(screenVertices[7].y), color);
+
+	// 面
+	Novice::DrawLine(
+	    int(screenVertices[0].x), int(screenVertices[0].y), int(screenVertices[4].x),
+	    int(screenVertices[4].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[1].x), int(screenVertices[1].y), int(screenVertices[5].x),
+	    int(screenVertices[5].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[2].x), int(screenVertices[2].y), int(screenVertices[6].x),
+	    int(screenVertices[6].y), color);
+	Novice::DrawLine(
+	    int(screenVertices[3].x), int(screenVertices[3].y), int(screenVertices[7].x),
+	    int(screenVertices[7].y), color);
+}
 
 
-const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3";
+bool IsCollision(const AABB& a, const Sphere& s1) {
+	Vector3 closesetPoint{
+	    std::clamp(s1.center.x, a.min.x, a.max.x), std::clamp(s1.center.y, a.min.y, a.max.y),
+	    std::clamp(s1.center.z, a.min.z, a.max.z)};
+	float distance = Length(Vec3Subtract(closesetPoint, s1.center));
+
+	if (distance <= s1.radius) {
+
+		return true;
+	}
+
+	return false;
+}
+
+
+const char kWindowTitle[] = "LE2D_18_ニヘイリュウダイ_MT3_02_06";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -139,6 +232,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	AABB aabb1{
+	    .min{-0.5f, -0.5f, -0.5f},
+	    .max{0.0f,  0.0f,  0.0f },
+	};
+
+	Sphere sphere = {
+	    {0.0f, 0.0f, 0.0f},
+        0.5f
+    };
+
+	uint32_t color = WHITE;
+
+	Vector3 rotate{0, 0, 0};
+	Vector3 translate{0, 0, 0};
+	Vector3 cameraTranslate{0.0f, 1.9f, -5.49f};
+	Vector3 cameraRotate{0.26f, 0.0f, 0.0f};
+
+	int mouseMovePosX = 0;
+	int mouseMovePosY = 0;
+
+	const float move = 0.01f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -153,6 +268,64 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		// カメラ
+		if (keys[DIK_SPACE]) {
+			Novice::GetMousePosition(&mouseMovePosX, &mouseMovePosY);
+
+			if (mouseMovePosX > 1280 / 2) {
+				cameraRotate.y += move;
+			} else if (mouseMovePosX < 1280 / 2) {
+				cameraRotate.y -= move;
+			}
+			
+		}
+
+		if (keys[DIK_W]) {
+			cameraTranslate.y += move;
+		} else if (keys[DIK_S]) {
+			cameraTranslate.y -= move;
+		} else if (keys[DIK_D]) {
+			cameraTranslate.x += move;
+		} else if (keys[DIK_A]) {
+			cameraTranslate.x -= move;
+		}
+
+		uint32_t value = Novice::GetWheel();
+
+		if (value > 0) {
+			cameraTranslate.z += move * 10;
+		} else if (value < 0) {
+			cameraTranslate.z -= move * 10;
+		}
+
+		// 各行列の計算
+		Matrix4x4 worldMatrix = MakeAffineMatrix({1.0f, 1.0f, 1.0f}, rotate, translate);
+
+		Matrix4x4 cameraMatrix =
+		    MakeAffineMatrix({1.0f, 1.0f, 1.0f}, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+
+		Matrix4x4 projecttionMatrix = MakePerspectiveFovMatrix(
+		    0.45f, float(1280) / float(720), 0.1f, 100.0f);
+		// VPMatrixを作る。同次クリップ空間
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projecttionMatrix);
+		// ViewportMatrixを作る
+		Matrix4x4 viewportMatrix =
+		    MakeViewportMatrix(0, 0, float(1280), float(720), 0.0f, 1.0f);
+
+		aabb1.min.x = (std::min)(aabb1.min.x, aabb1.max.x);
+		aabb1.max.x = (std::max)(aabb1.min.x, aabb1.max.x);
+		aabb1.min.y = (std::min)(aabb1.min.y, aabb1.max.y);
+		aabb1.max.y = (std::max)(aabb1.min.y, aabb1.max.y);
+
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("CamerRotate", &cameraRotate.x, 0.01f);
+
+		ImGui::DragFloat3("aabb1.min", &aabb1.min.x, 0.01f);
+		ImGui::DragFloat3("aabb1.max", &aabb1.max.x, 0.01f);
+	
+		ImGui::End();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -160,6 +333,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		if (IsCollision(aabb1, sphere)) {
+			color = RED;
+		} else {
+			color = WHITE;
+		}
+
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+
+		DrawAABB(aabb1, viewProjectionMatrix, viewportMatrix, color);
+		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, WHITE);
+
 
 		///
 		/// ↑描画処理ここまで
